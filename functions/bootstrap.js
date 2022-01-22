6db5fc48d8bd13391a19100c3b724c99450fe4e4
artistic-differences <ross_smurfille@yahoo.co.uk>
2022-01-22 12:09:13 +0000

    removed errant bootstrap

--- functions/bootstrap.js
+++
@@ -1,9 +0,0 @@
-const { addMiddleware } = require('../utils/security');
-const { addAdminUser } = require('../fixtures/addAdminUser')
-
-module.exports = async (req, res, next) => {
-  console.log('bootstrap started');
-  addMiddleware(req, res, next);
-  await addAdminUser();
-  res.status(200).send(`Bootstrap Completed`);
-};
